9e7fe2b0e9f72914b21e602918fc15db6dda0171
Destroycok <cristhian_manco@hotmail.com>
2023-08-01 17:03:12 -0500

    version 2.0

---
+++ app/src/main/java/com/example/uploadimage/Constantes.kt
@@ -0,0 +1,5 @@
+package com.example.uploadimage
+
+object Constantes {
+    const val BASE_URL = "http://192.168.101.18/upload_image/"
+}
---
+++ app/src/main/java/com/example/uploadimage/ReconocimientoFacial.kt
@@ -0,0 +1,16 @@
+package com.example.uploadimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReconocimientoFacial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reconocimiento_facial)
+
+    }
+
+
+
+
+}
